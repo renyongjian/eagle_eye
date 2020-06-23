d963bfdea029c8ad27f6b5defc589b22186d617c
renyongjian <renyongjian@github.com>
2020-06-23 21:59:55 +0800

    添加了背景图片还有按键

--- src/app_main.c
+++ src/app_main.c
@@ -6,14 +6,14 @@
 extern int eagle_eye_lcd_init();
 int main()
 {
-	int lcd_w =800;
-	int lcd_h = 480;
+	int lcd_w =455;
+	int lcd_h = 787;
 	char res_root[MAX_PATH + 1];
 	char app_root[MAX_PATH + 1];
 	path_app_root(app_root);
 	memset(res_root, 0x00, sizeof(res_root));
 	path_build(res_root, MAX_PATH, app_root, "res", NULL);
-	tk_init(lcd_w, lcd_h, APP_MOBILE, NULL, res_root);
+	tk_init(lcd_w, lcd_h, APP_MOBILE, "EagleEye", res_root);
 
 	FV_TRACE("AWTK_DEBUG--->");
 	/* 初始化资源 */
