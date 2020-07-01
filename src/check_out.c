00a7ca2ff5da1bc76ddc90b047e4c92f9458092c
renyongjian <renyongjian@github.com>
2020-07-01 23:59:14 +0800

    添加了一个写死的list，rich文本的使用了解

--- src/check_out.c
+++ src/check_out.c
@@ -2,6 +2,14 @@
 #include <time.h>
 #define RENYJ_TRACE( format, args... )do {fprintf(stderr,  "RENYJ_TRACE >>> %s->%s()->line.%d : " format "\n", __FILE__, __FUNCTION__, __LINE__, ##args);}while(0)
 
+int question_index = 0;
+char *question_list[]=
+{
+    "python 如何实现树状结构?",
+    "Unix 编程艺术中对我来说最实用的原则是哪个?",
+    "当前阶段能够最快速提升自己的方向是哪三个?",
+    "今天的工作状态是否满意?为什么?",
+};
 
 static ret_t on_close(void* ctx, event_t* e) {
   widget_t* win = (widget_t*)ctx;
@@ -9,6 +17,21 @@ static ret_t on_close(void* ctx, event_t* e) {
   return window_close(win);
 }
 
+
+static ret_t on_next(void *ctx,event_t *e)
+{
+    widget_t* win = (widget_t*)ctx;
+    (void)e;
+    question_index++;
+    question_index = question_index%4;
+    RENYJ_TRACE("NEXT_DEBUG--->win->name=%s",win->name);
+    widget_t *display_widget = widget_lookup(win,"check_out_show_text",1);
+    //rich_text_set_prop(display_widget,"text",);
+    char string[128]="";
+    snprintf(string,128,"<font size=\"40\">%s</font>",question_list[question_index]);
+    rich_text_set_text(display_widget, string);
+    return 0;
+}
 
 static ret_t init_widget(void* ctx, const void* iter)
 {
@@ -20,6 +43,8 @@ static ret_t init_widget(void* ctx, const void* iter)
         if (tk_str_eq(name, "next")) 
         {
             RENYJ_TRACE("AWTK_DEBUG--->");
+            widget_t* win = widget_get_window(widget);
+            widget_on(widget, EVT_CLICK, on_next, win);
         }
         else if(tk_str_eq(name, "exit"))
         {
